c2d1d07e64766615c334137a7b76521870a76693
lucymk <lucy.m.king92@gmail.com>
2019-11-30 22:04:20 +0000

    add gatsby framework with styled components and tailwind

---
+++ gatsby-config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require("postcss-import")(), // allows you to use @import
+          require("tailwindcss")(), // taaaailwind
+          require("autoprefixer")(), // adds vendor prefixes like -webkit or -moz to your css for better support
+          require("postcss-custom-media")(), // can set media queries as variables
+          require("postcss-custom-properties")(), // allows you to use css variables e.g. var(--my-variable)
+          require("postcss-clean")(), // minifies the output css (i.e. removes all the spaces and comments)
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        pure: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-netlify`,
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
