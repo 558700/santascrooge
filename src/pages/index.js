a1267e439f272a010ca7f60d8eb43b11e9e641ff
lucymk <lucy.m.king92@gmail.com>
2019-11-30 23:27:49 +0000

    set  up CMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,44 @@
-import React from "react"
-import styled from "styled-components"
-import { Link } from "gatsby"
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
 
-import "../styles/index.css"
+import "../styles/index.css";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const Test = styled.p.attrs({
-  className: "italic text-black ",
-})``
+  className: "italic text-black "
+})``;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Test>HELLO</Test>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  <body>
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Test>HELLO</Test>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+    <script
+      dangerouslySetInnerHTML={{
+        _html: `if (window.netlifyIdentity) {
+                  window.netlifyIdentity.on("init", user => {
+                    if (!user) {
+                      window.netlifyIdentity.on("login", () => {
+                        document.location.href = "/admin/";
+                      });
+                    }
+                  });
+                }`
+      }}
+    />
+  </body>
+);
 
-export default IndexPage
+export default IndexPage;
