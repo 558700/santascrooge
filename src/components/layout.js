aa35de7d6f899d9cf2d795deb8ea775255620400
lucymk <lucy.m.king92@gmail.com>
2019-12-01 16:33:50 +0000

    removes unneeded comps

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,11 +29,6 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <Main>{children}</Main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     </>
   );
